1ee2e75b6ae1215c5ab84e074ed20cb086bba6c6
present me app <Ghadasub@outlook.com>
2024-10-02 09:45:22 +0300

    Initial Commit

---
+++ full/fullApp.swift
@@ -0,0 +1,17 @@
+//
+//  fullApp.swift
+//  full
+//
+//  Created by Ghada Alsubaie on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct fullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
